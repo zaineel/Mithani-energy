0d2fb9eb51891e28cee7efa690d9fe797c154e8f
zaineel <127276018+zaineel@users.noreply.github.com>
2025-05-11 14:23:46 -0500

    added file

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,6 @@ import {
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-export const runtime = "edge";
-
 export default function Home() {
   return (
     <div className='flex min-h-screen flex-col'>
